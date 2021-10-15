e234814862fac53224a1327b2a46b21225324979
KarimIsaac <90776600+KarimIsaac@users.noreply.github.com>
2021-10-15 14:43:07 +0200

    1

--- app.js
+++ app.js
@@ -5,41 +5,40 @@ var el = document.getElementsByTagName('li');
 
 
 btn.onclick = function(){
-    
+
     var txt = input.value;
     if(txt ==''){
         alert('du måste skriva något');
     }else{
-        li = document.createElement('li');
+        li = document.createElement('LI');
     li.innerHTML = txt;
     list.insertBefore(li,list.childNodes[0]);
     input.value = '';
     }
-    
-};
 
+    var myNodelist = document.getElementsByTagName("LI");
+    var i;
+    for (i = 0; i < myNodelist.length; i++) {
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      myNodelist[i].appendChild(span);
+    }
+
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+
+};
 
 list.onclick = function(ev){
     if(ev.target.tagName == 'LI'){
          ev.target.classList.toggle('checked');
     }
 };
-
-
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
